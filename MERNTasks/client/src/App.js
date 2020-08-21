7804a71c9daae15e8d0af30d74fe4bf65f708e73
Marcelo <Oviedo>
2020-08-21 17:55:03 -0300

    des

--- MERNTasks/client/src/App.js
+++ MERNTasks/client/src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
-import {login} from './components/auth/login'
+import {Login} from './components/auth/Login'
 import {NuevaCuenta} from './components/auth/NuevaCuenta'
 import {Projects} from './components/projects/Projects'
 
 
 function App() {
   return (
-      <h1>MERN Tasks</h1>
+
       <Router>
           <Switch>
-              <Router exact path="/" component={Login}> </Router>
+              <Router exact path="/" component={Login}/>
               <Router exact path="/nueva" component={NuevaCuenta} />
               <Router exact path="/projects" component={Projects} />
           </Switch> 
--- MERNTasks/client/src/components/auth/login.js
+++ MERNTasks/client/src/components/auth/login.js
@@ -3,7 +3,24 @@ import React from 'react';
 
 const Login = () => {
     return (
-        <h1>Login</h1>
+        <div className="form-usuario">
+              <div className="contenedor-form sombra-dark">
+              <h1>Inicio</h1>
+                <form>
+                    <div className="campo-form" >
+                        <label>usuario</label>
+
+                    </div>
+                    <div className="campo-form" >
+                        <label htmlFor="usuario">password</label>
+                        <input 
+                            type="text"
+                            ></input>
+                    </div>
+                </form>
+            </div>
+        </div>
+   
     )
 
 }
